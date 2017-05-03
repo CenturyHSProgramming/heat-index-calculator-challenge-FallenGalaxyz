b0314ef4b450efffd0706122a8aa301582d69ab3
RomeroPedroza_Joshua <romej097@hsd.k12.or.us>
2017-05-03 10:37:40 -0700

    adding successful code

--- HeatIndexCalculator.py
+++ HeatIndexCalculator.py
@@ -6,7 +6,14 @@
 
 # Define Function below
 # be sure to return an integer
+import HeatIndexCalculator
 
+def calculateHeatIndex(relativeHumidity, temperature):
+    RH = relativeHumidity
+    T = temperature
+
+        # using the formula from http://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
+    return round(-42.379 + 2.04901523*T + 10.14333127*RH - .22475541*T*RH - .00683783*T*T - .05481717*RH*RH + .00122874*T*T*RH + .00085282*T*RH*RH - .00000199*T*T*RH*RH)
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
